684ed8f0a856f83c365435207a25e3b5b7a1c62a
ezliu <evanliu@stanford.edu>
2017-11-12 02:14:05 -0800

    Processes successfully talk to each other

--- test.py
+++ test.py
@@ -5,9 +5,20 @@ import socket
 PORTS = [8080, 8081, 8082, 8083]
 
 class Client(asyncore.dispatcher_with_send):
+    def __init__(self, addr=None, sock=None):
+        if sock is not None:
+            asyncore.dispatcher_with_send.__init__(self, sock)
+        else:
+            asyncore.dispatcher_with_send.__init__(self)
+            self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connect(addr)
+
     def handle_read(self):
         data = self.recv(8192)
         print "Received: {}".format(data)
+
+    def handle_connect(self):
+        self.send("hello, world!")
 
 
 class Server(asyncore.dispatcher):
@@ -21,34 +32,19 @@ class Server(asyncore.dispatcher):
         self.listen(5)
         for i in xrange(index):
             print "Trying to connect to: {}".format(PORTS[i])
-            self.connect(('localhost', PORTS[i]))
+            client = Client(('localhost', PORTS[i]))
 
     def handle_accept(self):
         pair = self.accept()
         if pair is not None:
             sock, addr = pair
             print 'Incoming connection from %s' % repr(pair)
-            handler = Client(sock)
+            handler = Client(sock=sock)
             handler.send("Accepted connection from: {}".format(pair))
-
-
-class Client(asyncore.dispatcher_with_send):
-    def __init__(self):
-        asyncore.dispatcher_with_send.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.bind(('0.0.0.0', 8081))
-        self.listen(5)
-        self.connect(("127.0.0.1", 8080))
-
-    def handle_connect(self):
-        self.send("Hello, world!")
+            handler.send("We did it!")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("port_index", type=int)
 args = parser.parse_args()
-if args.port_index == 0:
-    server = Server('localhost', args.port_index)
-else:
-    client = Client()
-#server = Server('localhost', args.port_index)
+server = Server('localhost', args.port_index)
 asyncore.loop()
