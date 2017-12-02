9780d602981a546e5d0bb21bb4f16814bf376d57
ezliu <evanliu@stanford.edu>
2017-12-01 19:55:01 -0800

    Fix spacing

--- src/server.py
+++ src/server.py
@@ -8,38 +8,50 @@ class Server(object):
         """
         pass
 
-   def get_state_machine(self, msg):
-       """Returns the state machine associated with this msg, if it exists.
-       Otherwise it creates a StateMachine and returns it.
+    def get_state_machine(self, msg):
+        """Returns the state machine associated with this msg, if it exists.
+        Otherwise it creates a StateMachine and returns it.
 
-       Args:
-           msg (Message)
+        Args:
+            msg (Message)
 
-       Returns:
-           StateMachine | None: None if msg's timestamp is too old
-       """
-       pass
+        Returns:
+            StateMachine | None: None if msg's timestamp is too old
+        """
+        pass
 
-   @property
-   def messaging_service(self):
-       pass
+    @property
+    def id(self):
+        pass
 
-   @property
-   def threshold_encryption_service(self):
-       pass
+    @property
+    def port(self):
+        pass
 
-   @property
-   def signature_service(self):
-       pass
+    @property
+    def host(self):
+        pass
 
-   @property
-   def write_ahead_log(self):
-       pass
+    @property
+    def messaging_service(self):
+        pass
 
-   @property
-   def secrets_log(self):
-       pass
+    @property
+    def threshold_encryption_service(self):
+        pass
 
-   @property
-   def catchup_state_machine(self):
-       pass
+    @property
+    def signature_service(self):
+        pass
+
+    @property
+    def write_ahead_log(self):
+        pass
+
+    @property
+    def secrets_log(self):
+        pass
+
+    @property
+    def catchup_state_machine(self):
+        pass
