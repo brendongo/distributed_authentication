58f7c913b14ed7689e216f76031c89fec24e5c68
ezliu <evanliu@stanford.edu>
2017-12-03 03:51:03 -0800

    Add launch script

---
+++ src/launch.sh
@@ -0,0 +1,14 @@
+python messaging_service.py 0 > m0.txt 2>&1 &
+sleep 1
+python messaging_service.py 1 > m1.txt 2>&1 &
+sleep 1
+python messaging_service.py 2 > m2.txt 2>&1 &
+sleep 1
+python messaging_service.py 3 > m3.txt 2>&1 &
+sleep 1
+python messaging_service.py 4 > m4.txt 2>&1 &
+sleep 1
+python messaging_service.py 5 > m5.txt 2>&1 &
+sleep 1
+python messaging_service.py 6 > m6.txt 2>&1 &
+sleep 1
