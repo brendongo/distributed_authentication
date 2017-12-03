e2b67c804771e833aa87a6a5505b91cfde823592
Kenneth Xu <kennethxu@Kenneths-MacBook-Pro-3.local>
2017-12-02 16:51:02 -0800

    Don't read at every validate

--- src/signature_service.py
+++ src/signature_service.py
@@ -1,4 +1,4 @@
-from os import path
+from os import path, listdir
 from ecdsa import SigningKey, VerifyingKey, BadSignatureError
 
 CONFIG_DIR = "config"
@@ -7,25 +7,38 @@ VK_STR_LEN = 48
 SK_STR_LEN = 24
 
 class SignatureService(object):
+    sk = None
+
     def __init__(self, server_id):
         """
         Args:
             id (int):
         """ 
-        self.config_filename = path.join(CONFIG_DIR, str(server_id) + CONFIG_SUFFIX)
-
-        if not path.isfile(self.config_filename):
+        # Create your own config file if it doesn't exist
+ 
+        self.server_id = server_id
+        own_config_filename = path.join(CONFIG_DIR, str(server_id) + CONFIG_SUFFIX)
+        if not path.isfile(own_config_filename):
             sk = SigningKey.generate()
             vk = sk.get_verifying_key()
-            with open(self.config_filename, "w") as config_file:
+            with open(own_config_filename, "w") as config_file:
                 config_file.write(vk.to_string())
                 config_file.write(sk.to_string())
 
-        with open(self.config_filename) as config_file:
-            self.vk = VerifyingKey.from_string(
-                        config_file.read(VK_STR_LEN))
-            self.sk = SigningKey.from_string(
-                        config_file.read(SK_STR_LEN))
+        # Look for all config files in the directory
+        self.vks = {}
+        for filename in listdir(CONFIG_DIR):
+            if filename.endswith(CONFIG_SUFFIX):
+                other_server_id = int(filename[0])
+                with open(path.join(CONFIG_DIR, filename)) as config_file:
+                    self.vks[other_server_id] = VerifyingKey.from_string(
+                                                    config_file.read(VK_STR_LEN))
+                    # Remember your own secret key
+                    if other_server_id == server_id:
+                        self.sk = SigningKey.from_string(
+                                    config_file.read(SK_STR_LEN))
+
+        assert self.sk # Make sure that you know your own secret key
 
     def sign(self, msg):
         """Sign message with private key
@@ -49,17 +62,25 @@ class SignatureService(object):
         Returns:
             bool
         """
-        sender_config_filename = path.join(CONFIG_DIR, str(sender) + CONFIG_SUFFIX)
-        if not path.isfile(sender_config_filename):
-            return False
-
-        with open(sender_config_filename) as sender_config_file:
-            vk = VerifyingKey.from_string(sender_config_file.read(VK_STR_LEN))
-            try:
-                vk.verify(signature, msg)
-            except BadSignatureError:
+        # If we do not have the sender on file, look for their config file
+        if sender not in self.vks:
+            sender_config_filename = path.join(CONFIG_DIR, str(sender) + CONFIG_SUFFIX)
+            if path.isfile(sender_config_filename):
+                with open(sender_config_filename) as config_file:
+                    self.vks[sender] = VerifyingKey.from_string(
+                                            config_file.read(VK_STR_LEN))
+            else:
+                print ("Server %r cannot find server %r in signature config files."
+                        %(self.server_id, sender))
                 return False
-            return True
+
+        # Perform validation
+        vk = self.vks[sender]
+        try:
+            vk.verify(signature, msg)
+        except BadSignatureError:
+            return False
+        return True
 
 # TEST CODE
 
