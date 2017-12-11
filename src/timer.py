f79f5d61ccab5059bdba24e9fb290c546fac375b
ezliu <evanliu@stanford.edu>
2017-12-10 16:41:22 -0800

    Add timer

---
+++ src/timer.py
@@ -0,0 +1,19 @@
+import time
+
+
+class Timer(object):
+    def __init__(self, num_calls, filename):
+        time.sleep(1)
+        self._num_calls = num_calls
+        self._start = time.time()
+        self._calls = []
+
+    def call(self, msg):
+        self._num_calls -= 1
+        self._calls.append(time.time() - self._start)
+        if self._num_calls == 0:
+            end = time.time()
+            total_time = end - self._start
+            print "{} TOTAL TIME: {} {}".format(
+                    "=" * 20, total_time, "=" * 20)
+            print self._calls
